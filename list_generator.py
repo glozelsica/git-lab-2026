687b6cb575eae9f7ac409e1dd94db7956e4bbf9c
Valeria <egarminavaleria830@gmail.com>
2026-03-05 21:04:45 +0300

    Создан файл с поэлементным заполнением списка

---
+++ list_generator.py
@@ -0,0 +1,12 @@
+import random
+
+# Поэлементное заполнение списка
+count = 14
+min_val = 5
+max_val = 400
+numbers = []
+
+for i in range(count):
+    numbers.append(random.randint(min_val, max_val))
+
+print(numbers)
