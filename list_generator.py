fc530f2ed94721ed41c87660bab8d936d118ee60
Valeria <egarminavaleria830@gmail.com>
2026-03-05 21:47:00 +0300

    Удаление кода

--- list_generator.py
+++ list_generator.py
@@ -1,13 +1,1 @@
-import random
 
-# Поэлементное заполнение списка
-count = 14
-min_val = 5
-max_val = 400
-numbers = []
-
-for i in range(count):
-    numbers.append(random.randint(min_val, max_val))
-
-print(numbers)
-# This is a comment
